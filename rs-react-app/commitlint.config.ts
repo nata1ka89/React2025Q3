2fbf087d9d0df8b6a4870eb20ef63deb60614ac8
nata1ka <nata1ka.yurevich@gmail.com>
2025-07-12 18:08:47 +0300

    chore: setup lintstaged, stylelint, commit-msg

---
+++ rs-react-app/commitlint.config.ts
@@ -0,0 +1,25 @@
+import { RuleConfigSeverity } from '@commitlint/types';
+
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      RuleConfigSeverity.Error,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        'init',
+      ],
+    ],
+  },
+};
