7070bbf10c36228bf512bd8e79d6dd735a981dde
nata1ka <nata1ka.yurevich@gmail.com>
2025-07-18 18:00:31 +0300

    refactor: move mock data to a separate file

--- rs-react-app/src/__ tests __/card.test.tsx
+++ rs-react-app/src/__ tests __/card.test.tsx
@@ -1,22 +1,11 @@
 import '@testing-library/jest-dom';
 import Card from '../components/card';
 import { render, screen } from '@testing-library/react';
+import { mockItems } from '../setupTests';
 
 describe('Card Component', () => {
-  const mockItem = {
-    name: 'Darth Vader',
-    url: 'https://www.swapi.tech/api/people/4',
-    gender: 'male',
-    height: '202',
-    mass: '136',
-    birth_year: '41.9BBY',
-    skin_color: 'white',
-    hair_color: 'none',
-    eye_color: 'yellow',
-    homeworld: 'https://www.swapi.tech/api/planets/1',
-  };
   test('renders correct number of cards', () => {
-    render(<Card {...mockItem} />);
+    render(<Card {...mockItems[0]} />);
     expect(screen.getByText('Darth Vader')).toBeInTheDocument();
   });
 });
--- rs-react-app/src/setupTests.ts
+++ rs-react-app/src/setupTests.ts
@@ -4,3 +4,30 @@ global.fetch = jest.fn(() =>
     json: () => Promise.resolve({ data: 'mocked data' }),
   })
 ) as jest.Mock;
+
+export const mockItems = [
+  {
+    name: 'Darth Vader',
+    url: 'https://www.swapi.tech/api/people/4',
+    gender: 'male',
+    height: '202',
+    mass: '136',
+    birth_year: '41.9BBY',
+    skin_color: 'white',
+    hair_color: 'none',
+    eye_color: 'yellow',
+    homeworld: 'https://www.swapi.tech/api/planets/1',
+  },
+  {
+    name: 'Dexter Jettster',
+    url: 'https://www.swapi.tech/api/people/71',
+    gender: 'male',
+    height: '198',
+    mass: '102',
+    birth_year: 'unknown',
+    skin_color: 'brown',
+    hair_color: 'none',
+    eye_color: 'yellow',
+    homeworld: 'https://www.swapi.tech/api/planets/55',
+  },
+];
