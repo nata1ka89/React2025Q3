f34c8b8b2b581f5b3dbb683d799275e56a825d8b
nata1ka <nata1ka.yurevich@gmail.com>
2025-07-18 17:46:42 +0300

    feat: add tests for the header, app, error-boundary components

---
+++ rs-react-app/src/__ tests __/app.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import App from '../App';
+
+describe('App Component', () => {
+  test('renders App without crashing', async () => {
+    render(<App />);
+    const main = screen.getByRole('main');
+    expect(main).toBeInTheDocument();
+
+    const header = screen.getByRole('banner');
+    expect(header).toBeInTheDocument();
+  });
+});
