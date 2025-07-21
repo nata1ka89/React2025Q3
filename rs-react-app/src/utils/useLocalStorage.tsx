3ca75e5fa67c8d95916c9172d2a54844f5d5d82b
nata1ka <nata1ka.yurevich@gmail.com>
2025-07-21 17:22:36 +0300

    refactor: refactor search component, create custom hook for localStorage operations

---
+++ rs-react-app/src/utils/useLocalStorage.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+export default function useLocalStorage(key: string) {
+  const [storedValue, setStoredValue] = useState(() => {
+    return localStorage.getItem('searchValue') || '';
+  });
+
+  const setValue = (value: string) => {
+    try {
+      localStorage.setItem(key, value);
+      setStoredValue(value);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return [storedValue, setValue] as const;
+}
