883d908f24a759d49dfa6a8cd3ec1cf4f1f1bdc8
nata1ka <nata1ka.yurevich@gmail.com>
2025-07-21 17:35:22 +0300

    refactor: refactor card component

--- rs-react-app/src/components/card.tsx
+++ rs-react-app/src/components/card.tsx
@@ -1,34 +1,32 @@
 import React from 'react';
 import type { DescriptionProps } from '../types/types';
 
-class Card extends React.Component<DescriptionProps> {
-  render() {
-    const { ...item } = this.props;
+const Card: React.FC<DescriptionProps> = (props) => {
+  const { ...item } = props;
 
-    if ('gender' in item) {
-      return (
-        <div className="card">
-          <h3>{item.name}</h3>
-          <p>URL: {item.url}</p>
-          <p>Gender: {item.gender}</p>
-          <p>Height: {item.height}</p>
-          <p>Mass: {item.mass}</p>
-          <p>Birth Year: {item.birth_year}</p>
-          <p>Skin Color: {item.skin_color}</p>
-          <p>Hair Color: {item.hair_color}</p>
-          <p>Eye Color: {item.eye_color}</p>
-          <p>Home world: {item.homeworld}</p>
-        </div>
-      );
-    } else {
-      return (
-        <div className="card">
-          <h3>{item.name}</h3>
-          <p>URL: {item.url}</p>
-        </div>
-      );
-    }
+  if ('gender' in item) {
+    return (
+      <div className="card">
+        <h3>{item.name}</h3>
+        <p>URL: {item.url}</p>
+        <p>Gender: {item.gender}</p>
+        <p>Height: {item.height}</p>
+        <p>Mass: {item.mass}</p>
+        <p>Birth Year: {item.birth_year}</p>
+        <p>Skin Color: {item.skin_color}</p>
+        <p>Hair Color: {item.hair_color}</p>
+        <p>Eye Color: {item.eye_color}</p>
+        <p>Home world: {item.homeworld}</p>
+      </div>
+    );
+  } else {
+    return (
+      <div className="card">
+        <h3>{item.name}</h3>
+        <p>URL: {item.url}</p>
+      </div>
+    );
   }
-}
+};
 
 export default Card;
