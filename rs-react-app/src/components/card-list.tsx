84ee40caff465f605a8ba5e5dd409a4d3444a224
nata1ka <nata1ka.yurevich@gmail.com>
2025-07-21 17:37:56 +0300

    refactor: refactor card-list component

--- rs-react-app/src/components/card-list.tsx
+++ rs-react-app/src/components/card-list.tsx
@@ -2,18 +2,16 @@ import React from 'react';
 import Card from './card';
 import type { CardListProps } from '../types/types';
 
-class CardList extends React.Component<CardListProps> {
-  render() {
-    const { items } = this.props;
+const CardList: React.FC<CardListProps> = (props) => {
+  const { items } = props;
 
-    return (
-      <div className="card-list">
-        {items.map((item, index) => (
-          <Card key={index} {...item} />
-        ))}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="card-list">
+      {items.map((item, index) => (
+        <Card key={index} {...item} />
+      ))}
+    </div>
+  );
+};
 
 export default CardList;
