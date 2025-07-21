980c50174e078f25f76ca1adf89c5ee0c4ee3f16
nata1ka <nata1ka.yurevich@gmail.com>
2025-07-21 20:49:29 +0300

    feat: add pagination

--- rs-react-app/src/components/main.tsx
+++ rs-react-app/src/components/main.tsx
@@ -11,55 +11,83 @@ const Main: React.FC<MainProps> = ({ searchValue }) => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
   const [throwError, setThrowError] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPage, setTotalPage] = useState(1);
 
-  const fetchData = useCallback(async (searchValue: string = '') => {
-    setLoading(true);
-    setError(null);
-    const url = searchValue
-      ? `https://swapi.tech/api/people/?name=${searchValue}`
-      : `https://swapi.tech/api/people`;
+  const fetchData = useCallback(
+    async (searchValue: string = '', page: number = 1) => {
+      setLoading(true);
+      setError(null);
+      const limit = 10;
+      const url = searchValue
+        ? `https://swapi.tech/api/people/?name=${searchValue}&page=${page}&limit=${limit}`
+        : `https://swapi.tech/api/people?page=${page}&limit=${limit}`;
 
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(`Error: ${response.status}`);
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status}`);
+        }
+        const data = await response.json();
+        if (!data.results && !data.result) {
+          throw new Error('No data found');
+        }
+        if (data.results) {
+          setItems(data.results);
+          setTotalPage(data.total_pages || 1);
+        } else {
+          const items = data.result.map(
+            (item: CharacterResult) => item.properties
+          );
+          setItems(items);
+          setTotalPage(data.total_pages || 1);
+        }
+      } catch (error: unknown) {
+        if (error instanceof Error) {
+          setError(error.message);
+        }
+      } finally {
+        setLoading(false);
       }
-      const data = await response.json();
-      if (!data.results && !data.result) {
-        throw new Error('No data found');
-      }
-      if (data.results) {
-        setItems(data.results);
-      } else {
-        const items = data.result.map(
-          (item: CharacterResult) => item.properties
-        );
-        setItems(items);
-      }
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        setError(error.message);
-      }
-    } finally {
-      setLoading(false);
-    }
-  }, []);
+    },
+    []
+  );
 
   useEffect(() => {
     const valueToSearch =
       searchValue || localStorage.getItem('searchValue') || '';
-    fetchData(valueToSearch);
-  }, [searchValue, fetchData]);
+    fetchData(valueToSearch, currentPage);
+  }, [searchValue, fetchData, currentPage]);
 
   if (throwError) {
     throw new Error('Test error from Main component');
   }
+
+  const handleNextPage = () => {
+    if (currentPage < totalPage) {
+      setCurrentPage((prevPage) => prevPage + 1);
+    }
+  };
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage((prevPage) => prevPage - 1);
+    }
+  };
   return (
     <main className="main">
       {loading && <div>Loading...</div>}
       {error && <div className="error-message">{error}</div>}
       <button onClick={() => setThrowError(true)}>Throw Error</button>
       <CardList items={items} />
+      <div className="pagination">
+        <button onClick={handlePrevPage} disabled={currentPage === 1}>
+          Prev
+        </button>
+        <button onClick={handleNextPage} disabled={currentPage === totalPage}>
+          Next
+        </button>
+      </div>
     </main>
   );
 };
