66d856d45a61511a2eac4cc53ee562e366da1213
nata1ka <nata1ka.yurevich@gmail.com>
2025-07-22 16:21:18 +0300

    feat: add detail component, change card and card-list component

--- rs-react-app/src/components/card-list.tsx
+++ rs-react-app/src/components/card-list.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import Card from './card';
 import type { CardListProps } from '../types/types';
 
-const CardList: React.FC<CardListProps> = (props) => {
-  const { items } = props;
-
+const CardList: React.FC<CardListProps> = ({ items, onSelect }) => {
   return (
     <div className="card-list">
       {items.map((item, index) => (
-        <Card key={index} {...item} />
+        <div key={index} onClick={() => onSelect(item)}>
+          <Card {...item} />
+        </div>
       ))}
     </div>
   );
